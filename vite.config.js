c5ea6e4993c299ed4dcf2b440cf616182f8f4736
Sean Soriano <arkadatax03@gmail.com>
2026-04-17 16:37:24 +0800

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import Generouted from '@generouted/react-router/plugin'
 import tailwindcss from '@tailwindcss/vite'
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss(), Generouted()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
